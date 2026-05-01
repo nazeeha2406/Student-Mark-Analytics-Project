ab0ca80b21cfab16def301b57a7c9141402480d2
Nazeeha <nazeeha2406@gmail.com>
2026-05-01 16:01:49 +0530

    Connected project to AWS S3 for cloud storage

--- modules/load_data.py
+++ modules/load_data.py
@@ -1,13 +1,15 @@
 import pandas as pd
 import os
 
-CSV_PATH = os.path.join(os.path.dirname(__file__), "..", "data", "Studentdata.csv")
+BUCKET_NAME = "student-mark-analytics"
+FILE_KEY = "Studentdata.csv"
+S3_PATH = f"s3://{BUCKET_NAME}/{FILE_KEY}"
 
 SUBJECTS = ["Tamil", "English", "Maths", "Physics", "Chemistry", "Biology"]
 
 def load():
-    df = pd.read_csv(CSV_PATH)
+    df = pd.read_csv(S3_PATH)
     return df
 
 def save(df):
-    df.to_csv(CSV_PATH, index=False)
+    df.to_csv(S3_PATH, index=False)
